a446d2288cd1cb0ae30d5b37edb0209d45f291d6
MC504 <vincentm.work.2023@gmail.com>
2024-05-13 16:34:28 +0800

    added database

---
+++ backend/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, String, Integer, Date, Time, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine("sqlite:///backend/db_todo.db", echo=True)
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = "todo"
+	
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	todo = Column(String)
+	date = Column(Date)
+	time = Column(Time)
+	isDone = Column(Boolean)
+
+Base.metadata.create_all(engine)
