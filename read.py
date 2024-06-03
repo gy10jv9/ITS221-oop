84e6d176c35b720dcbe956363fc7f6dddc977e5f
Vincent Manalo <146041946+gy10jv9@users.noreply.github.com>
2024-06-03 13:54:40 +0800

    added spaces

--- read.py
+++ read.py
@@ -9,3 +9,4 @@ users = session.query(User).all()
 
 for user in users:
     print(f"User ID: {user.ssn}, First Name: {user.firstname},  Last Name: {user.lastname}, Gender: {user.gender}, Age: {user.age}")
+        print(f"User ID: {user.ssn}, First Name: {user.firstname},  Last Name: {user.lastname}, Gender: {user.gender}, Age: {user.age}")     
