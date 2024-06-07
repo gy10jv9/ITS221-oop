298422972ec0d440f2ed6d73768d67863d92985c
Vincent Manalo <146041946+gy10jv9@users.noreply.github.com>
2024-06-07 11:52:24 +0800

    Finals exam unstable (#4)
    * added loop
    
    * added isdone
    
    * Refactor app.py and database.py to use consistent naming conventions for the 'isdone' attribute
    
    * lessen the code
    
    * removed line
    
    * Refactor code to use consistent naming conventions for the 'author' attribute
    
    * added relationships
    
    * moved functions
    
    * update
    
    * damo pa ni error
    
    * fixe missing codes
    
    * added print
    
    * updated update function
    
    * finished
    
    ---------
    
    Co-authored-by: JKairu <micadario0926@gmail.com>

--- app.py
+++ app.py
@@ -1,38 +1,79 @@
-from sqlalchemy.orm import sessionmaker
-from backend.database import Todo, engine
+from backend.database import Task, Author
 from datetime import datetime
-from update import update_todo
 
 def menu(choice):
     match choice:
         case 1:
-            Session = sessionmaker(bind=engine)
-            session = Session()
-            
-            name = input("Enter your name: ")
-            todo = input("Enter your task: ")
-            date = datetime.strptime(input("Enter your due date (YYYY-MM-DD): "), '%Y-%m-%d').date()
-            time = datetime.strptime(input("Enter your due time (HH:MM:SS): "), '%H:%M:%S').time()
-            
-            user = Todo(name=name, todo=todo, date=date, time=time)
-            session.add(user)
-            session.commit()
+            Author_Obj = Author()
+            author_id = Author_Obj.add(input("Enter your name: "))
+
+            task = {
+                'author_id': author_id,
+                'task': input("Enter your task: "),
+                'date': datetime.strptime(input("Enter your due date (YYYY-MM-DD): "), '%Y-%m-%d').date(),
+                'time': datetime.strptime(input("Enter your due time (HH:MM): "), '%H:%M').time(),
+                'isdone': False
+            }
+
+            Task_Obj = Task()
+            Task_Obj.add(task)
             
         case 2:
-            print("Searching...")
+            Task_Obj = Task()
+            Task_Obj.viewall()
 
-        case 3: #update.py :P
+        case 3:
             todo_id = int(input("Enter the ID of the todo item you want to update: "))
-            new_todo = input("Enter the updated task: ")
-            update_todo(todo_id, new_todo)
-            
-        case 4:
-            print("Exiting...")
-                        
-choice = input("[1] Add todo\n"
-            "[2] View all todo list\n"
-            "[3] Update todo\n"
-            "[4] Exit\n"
-            "Enter number of your choice: ")
+            new_task = input("Enter the updated task: ")
+            new_date = datetime.strptime(input("Enter the updated due date (YYYY-MM-DD): "), '%Y-%m-%d').date()
+            new_time = datetime.strptime(input("Enter the updated due time (HH:MM): "), '%H:%M').time()
+            isdone = input("Is the task done? (y/n): ").lower() == 'y'
 
-menu(int(choice))
+            Task_Obj = Task()
+            Task_Obj.update({
+                'id': todo_id,
+                'task': new_task,
+                'date': new_date,
+                'time': new_time,
+                'isdone': isdone
+            })
+          
+        case 4: #delete.py :P
+            Task_Obj = Task()
+            todo_id = int(input("Enter the ID of the todo item you want to delete: "))
+            Task_Obj.delete(todo_id)  
+            
+        case 5: # para sa search
+            print("[1] Search by task\n"
+                "[2] Search by author\n"
+                "[3] Search by date\n"
+                "[4] Exit\n")
+    
+            choice = int(input("Enter number of your choice: "))
+            Task_Obj = Task()
+            
+            match choice:
+                case 1: 
+                    task = input("Enter task to search: ")
+                    Task_Obj.searchbyTask(task)
+                case 2:
+                    author = input("Enter author to search: ")
+                    Task_Obj.searchbyAuthor(author)
+                case 3:
+                    date = datetime.strptime(input("Enter your due date (YYYY-MM-DD): "), '%Y-%m-%d').date()
+                    Task_Obj.searchbyDate(date)
+            
+        case 6: # para sa search
+            exit("Exiting...")
+
+while True:
+    print("[1] Add todo\n"
+        "[2] View all todo list\n"
+        "[3] Update todo\n"
+        "[4} Delete todo\n"
+        "[5} Search\n"
+        "[6] Exit\n")
+     
+    choice = input("Enter number of your choice: ")
+
+    menu(int(choice))
--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,91 @@
-from sqlalchemy import create_engine, Column, String, Integer, Date, Time, Boolean
+from sqlalchemy import create_engine, Column, String, Integer, Date, Time, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///backend/db_todo.db", echo=True)
+engine = create_engine("sqlite:///backend/db_todo.db", echo=False)
 Base = declarative_base()
 
-class Todo(Base):
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Task(Base):
 	__tablename__ = "tbl_todo"
 	
 	id = Column(Integer, primary_key=True)
-	name = Column(String)
-	todo = Column(String)
+	author_id = Column(Integer, ForeignKey('tbl_authors.id'))
+	task = Column(String)
 	date = Column(Date)
 	time = Column(Time)
-	# isDone = Column(Boolean)
+	isdone = Column(Boolean)
+ 
+	def add(self, payload):
+		todo = Task(author_id=payload["author_id"], task=payload["task"], date=payload["date"], time=payload["time"], isdone=False)
+		session.add(todo)
+		session.commit()
+  
+	def viewall(self):
+		tasks = session.query(Task).all()
+		for task in tasks:
+			author = session.query(Author).filter_by(id=task.author_id).first()
+			print(f"ID: {task.id}\nAuthor: {author.author}\nTask: {task.task}\nDate: {task.date}\nTime: {task.time}\nIs Done: {task.isdone}\n")
+   
+	def update(self, payload):
+		task = session.query(Task).filter_by(id=payload['id']).one_or_none()
+		if not task:
+			print("Todo item not found.")
+		else:
+			task.task = payload['task']
+			task.date = payload['date']
+			task.time = payload['time']
+			task.isdone = payload['isdone']
+			session.commit()
+			print("Todo item updated successfully!")
+
+	def delete(self, task_id):
+		task = session.query(Task).filter_by(id=task_id).first()
+		if task:
+			session.delete(task)
+			session.commit()
+			print(f"Task with ID {task_id} has been deleted.")
+		else:
+			print(f"Task with ID {task_id} not found") 
+            
+	def searchbyTask(self, search_term):
+		tasks = session.query(Task).filter(Task.task.contains(search_term)).all()
+		for task in tasks:
+			author = session.query(Author).filter_by(id=task.author_id).first()
+			print(f"ID: {task.id}\nAuthor: {author.author}\nTask: {task.task}\nDate: {task.date}\nTime: {task.time}\nIs Done: {task.isdone}\n")
+	
+	def searchbyAuthor(self, author):
+		author = session.query(Author).filter_by(author=author).first()
+		if author:
+			tasks = session.query(Task).filter(Task.author_id == author.id).all()
+			for task in tasks:
+				print(f"ID: {task.id}\nAuthor: {author.author}\nTask: {task.task}\nDate: {task.date}\nTime: {task.time}\nIs Done: {task.isdone}\n")
+		else:
+			print(f"Author {author} not found")
+
+	def searchbyDate(self, search_date):
+		tasks = session.query(Task).filter(Task.date == search_date).all() 
+		for task in tasks:
+			author = session.query(Author).filter_by(id=task.author_id).first()
+			print(f"ID: {task.id}\nAuthor: {author.author}\nTask: {task.task}\nDate: {task.date}\nTime: {task.time}\nIs Done: {task.isdone}\n")
+
+
+class Author(Base):
+	__tablename__ = "tbl_authors"
+	
+	id = Column(Integer, primary_key=True)
+	author = Column(String)
+ 
+	def add(self, payload):
+		author = session.query(Author).filter_by(author=payload).first()
+		if not author:
+			author = Author(author=payload)
+			session.add(author)
+			session.commit()
+   
+		return author.id
+	
 
 Base.metadata.create_all(engine)
